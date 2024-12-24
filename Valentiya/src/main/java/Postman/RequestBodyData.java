4a3f0f146975c6d7e7f6b0067d7d067c20a9de83
ValentiyaS <satyginavalentia@gmail.com>
2024-12-24 21:39:38 +0200

    Работа с Postman

---
+++ Valentiya/src/main/java/Postman/RequestBodyData.java
@@ -0,0 +1,20 @@
+package Postman;
+
+public class RequestBodyData {
+    private String name;
+    private String surname;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+}
